010a30598817ba7b9147b1fc2bb9deb5a27e670d
Meetesh Mehta <meetesh@Meeteshs-MacBook-Air.local>
2018-05-07 23:13:06 +0530

    directory

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ import api from './api';
 import config from './config';
 
 const server = express();
-server.use(express.static(__dirname+'/dock_frontend/build'));
+server.use(express.static('./dock_frontend/build'));
 server.get('/', (req,res) => {
-  res.sendFile(__dirname+'/dock_frontend/build/index.html');
+  res.sendFile('./dock_frontend/build/index.html');
 });
 server.use('/api', api);
 server.listen(config.port, config.host, () => console.log('API Server is live on '+config.getHost()));
