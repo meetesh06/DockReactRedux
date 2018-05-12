3a104725c103ca84c163770fad4c00aa5ddf3a86
OGIL <androidrajpoot@gmail.com>
2018-05-13 04:01:08 +0530

    minor fiex port

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
-  port: process.env.PORT || 7190,
-  host: process.env.HOST || '0.0.0.0',
+  port: process.env.PORT || 65534,
+  host: process.env.HOST || '127.0.0.1',
   getHost() {
     return 'http://'+this.host+':'+this.port;
   }
