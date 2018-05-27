df97e00f16a610a9d557516053425287c35da8a9
OGIL <androidrajpoot@gmail.com>
2018-05-27 19:09:00 +0530

    minor changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server = express();
 server.use(compression());
 server.set('view engine', 'ejs');
 server.use(express.static(path.join(__dirname, 'dock_frontend/build')));
-server.use(express.static(path.join(__dirname, 'api/events')));
+server.use(express.static(path.join(__dirname, 'api/media')));
 server.use('/', api);
 
 server.get('*', function(req, res) {
